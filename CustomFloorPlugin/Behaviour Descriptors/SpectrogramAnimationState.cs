b84b34652dde135934135d04110c11b2fae69657
Robyn <slovett@hotmail.co.uk>
2018-08-31 00:40:08 +0100

    SpectrogramAnimationState

---
+++ CustomFloorPlugin/Behaviour Descriptors/SpectrogramAnimationState.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace CustomFloorPlugin
+{
+    public class SpectrogramAnimationState : MonoBehaviour
+    {
+        public AnimationClip animationClip;
+        [Header("0: Low Frequency, 63 High Frequency")]
+        [Range(0,63)]
+        public int sample;
+        [Header("Use the average of all samples, ignoring specified sample")]
+        public bool averageAllSamples;
+
+        private Animation animation;
+        private BasicSpectrogramData spectrogramData;
+
+        public void setData(BasicSpectrogramData newData)
+        {
+            spectrogramData = newData;
+        }
+
+        void Update()
+        {
+            try
+            {
+                if (animationClip != null)
+                {
+                    animation = GetComponent<Animation>();
+                    if (animation == null)
+                    {
+                        animation = gameObject.AddComponent<Animation>();
+                        animation.AddClip(animationClip, "clip");
+                        animation.Play("clip");
+                        animation["clip"].speed = 0;
+                    }
+
+                    if (spectrogramData != null)
+                    {
+                        float average = 0.0f;
+                        for (int i = 0; i < 64; i++)
+                        {
+                            average += spectrogramData.ProcessedSamples[i];
+                        }
+                        average = average / 64.0f;
+
+                        float value = averageAllSamples ? average : spectrogramData.ProcessedSamples[sample];
+
+                        value = value * 5f;
+                        if (value > 1f)
+                        {
+                            value = 1f;
+                        }
+                        value = Mathf.Pow(value, 2f);
+
+                        animation["clip"].time = value * animation["clip"].length;
+
+                    }
+                }
+            } catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
---
+++ CustomFloorPlugin/Behaviour Managers/SpectrogramAnimationStateManager.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace CustomFloorPlugin
+{
+    public class SpectrogramAnimationStateManager : MonoBehaviour
+    {
+        List<SpectrogramAnimationState> animationStates;
+
+        private void BSSceneManagerOnActiveSceneChanged(Scene arg0, Scene arg1)
+        {
+            UpdateSpectrogramDataProvider();
+        }
+
+        private void OnEnable()
+        {
+            BSSceneManager.activeSceneChanged += BSSceneManagerOnActiveSceneChanged;
+            UpdateSpectrogramDataProvider();
+        }
+
+        private void OnDisable()
+        {
+            BSSceneManager.activeSceneChanged -= BSSceneManagerOnActiveSceneChanged;
+        }
+
+        public void UpdateAnimationStates()
+        {
+            if (animationStates == null) animationStates = new List<SpectrogramAnimationState>();
+
+            foreach (SpectrogramAnimationState spec in Resources.FindObjectsOfTypeAll(typeof(SpectrogramAnimationState)))
+            {
+                animationStates.Add(spec);
+            }
+        }
+
+        public void UpdateSpectrogramDataProvider()
+        {
+            BasicSpectrogramData[] datas = Resources.FindObjectsOfTypeAll<BasicSpectrogramData>();
+            if (datas.Length == 0) return;
+            
+            BasicSpectrogramData spectrogramData = datas.First();
+
+            foreach (SpectrogramAnimationState specAnim in animationStates)
+            {
+                specAnim.setData(spectrogramData);
+            }
+        }
+    }
+}

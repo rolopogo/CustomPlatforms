fab2e3190ba62e158fde5ef289709e90376c7fb4
Robyn <slovett@hotmail.co.uk>
2018-12-06 23:28:22 +0000

    Camera visibility selector first/third/default

---
+++ CustomFloorPlugin/Behaviour Descriptors/CameraVisibility.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace CustomFloorPlugin
+{
+    public class CameraVisibility : MonoBehaviour
+    {
+        public enum VisibilityMode { Default, HeadsetOnly, ThirdPersonOnly};
+        public VisibilityMode visibilityMode = VisibilityMode.Default;
+        public bool affectChildren = false;
+
+        // --------------------------- 
+
+        private void Awake()
+        {
+            int layer = gameObject.layer;
+
+            switch (visibilityMode)
+            {
+                case VisibilityMode.Default:
+                    return;
+                case VisibilityMode.HeadsetOnly:
+                    layer = CameraVisibilityManager.OnlyInHeadset;
+                    break;
+                case VisibilityMode.ThirdPersonOnly:
+                    layer = CameraVisibilityManager.OnlyInThirdPerson;
+                    break;
+            }
+            if (affectChildren)
+            {
+                SetChildrenToLayer(gameObject, layer);
+            }
+            else
+            {
+                gameObject.layer = layer;
+            }
+        }
+
+        // Recursively set the layer of an object and all children in its hierarchy
+        private void SetChildrenToLayer(GameObject gameObject, int layer)
+        {
+            gameObject.layer = layer;
+            foreach (Transform child in transform)
+            {
+                SetChildrenToLayer(child.gameObject, layer);
+            }
+        }
+    }
+}
---
+++ CustomFloorPlugin/Behaviour Managers/CameraVisibilityManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomFloorPlugin
+{
+    public class CameraVisibilityManager
+    {
+        public const int OnlyInThirdPerson = 3;
+        public const int OnlyInHeadset = 4;
+    }
+}

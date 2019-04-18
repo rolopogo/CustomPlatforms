383f465dd3fb24f514407f819f5eb5c04889f19d
brian91292 <8050523+brian91292@users.noreply.github.com>
2019-04-17 23:43:56 -0500

    Update TubeLight.cs

--- CustomFloorPlugin/Behaviour Descriptors/TubeLight.cs
+++ CustomFloorPlugin/Behaviour Descriptors/TubeLight.cs
@@ -77,8 +77,8 @@ namespace CustomFloorPlugin
                 // swap for MeshBloomPrePassLight
                 tubeBloomLight.gameObject.SetActive(false);
                 MeshBloomPrePassLight meshbloom = ReflectionUtil.CopyComponent(tubeBloomLight, typeof(TubeBloomPrePassLight), typeof(MeshBloomPrePassLight), tubeBloomLight.gameObject) as MeshBloomPrePassLight;
-                tubeBloomLight.gameObject.SetActive(true);
                 meshbloom.Init(tl.GetComponent<Renderer>());
+                tubeBloomLight.gameObject.SetActive(true);
                 Destroy(tubeBloomLight);
                 tubeBloomLight = meshbloom;
             }
